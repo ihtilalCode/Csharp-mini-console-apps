aeef6effc43587a68e09b57f70b7e3113d17963b
ihtilalCode <hilalnuraltuntas67@gmail.com>
2024-11-20 18:24:29 +0300

    Add files via upload
    Kullanıcıdan alınan rastgele üç açı değerinin üçgen oluşturup oluşturamayacağını söyler.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.Write("A değerini girin: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("B değerini girin: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("C değerini girin: ");
+        int c = Convert.ToInt32(Console.ReadLine());
+
+        int toplam = a + b + c;
+
+        if (toplam < 180)
+        {
+            Console.WriteLine("Bu bir üçgen değildir");
+        }
+        else if (toplam > 180)
+        {
+            Console.WriteLine("Bu bir üçgen değildir");
+        }
+        else
+        {
+            Console.WriteLine("Bu bir üçgendir");
+        }
+    }
+}
