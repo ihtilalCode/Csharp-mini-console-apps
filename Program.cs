f04f21561f14ee7bf63c3a2ecbef9943338e0a01
ihtilalCode <hilalnuraltuntas67@gmail.com>
2024-11-20 18:01:09 +0300

    Add files via upload
    Girilen sayının kendisine kadarki olan sayıların toplamını verir.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma10.hafta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir sayı giriniz: ");
+            int sayi =Convert.ToInt32(Console.ReadLine());
+
+            int toplam = 0;
+            for (int i = 0; i < sayi; i++)
+            {
+                toplam += i;
+            }  
+            Console.WriteLine(toplam);
+        }
+    }
+}
