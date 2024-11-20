5aa4ce8e7bda22dfb2a68fdb5a9a5c4f740047d0
ihtilalCode <hilalnuraltuntas67@gmail.com>
2024-11-20 18:13:40 +0300

    Add files via upload
    N haneli bir sayının basamaklarının n'inci üstlerinin toplamı, sayının kendisine eşitse, böyle sayılara Armstrong sayı denir.
    Girilen sayının Armstrong sayı olup olmadığını söyler.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma10.hafta_1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sayı giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            int basamakSayisi = sayi.ToString().Length;
+            int toplam = 0;
+            int geciciSayi = sayi;
+
+
+            while (geciciSayi > 0)
+            {
+                int basamakDegeri = geciciSayi % 10;
+                toplam += (int)Math.Pow(basamakDegeri, basamakSayisi);
+                geciciSayi /= 10;
+            }
+
+
+            if (toplam == sayi)
+            {
+                Console.WriteLine(sayi + " bir Armstrong sayısıdır.");
+            }
+            else
+            {
+                Console.WriteLine(sayi + " bir Armstrong sayısı değildir.");
+            }
+        }
+    }
+}
