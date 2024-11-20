cdd4cec4c7bc5976b083f92d6cdf4351c3f4fc7b
ihtilalCode <hilalnuraltuntas67@gmail.com>
2024-11-20 14:48:04 +0300

    Add files via upload
    Bir kenar uzunluğu kullanıcıdan alınan karenin alan hesabını yapar.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{ static void Main(string[] args)
+    {
+        Console.WriteLine("Karenin kenar uzunluğunu girin: ");
+        int kenar = Convert.ToInt32(Console.ReadLine());
+
+        int alan = kenar * kenar;
+
+        Console.WriteLine("Karenin alanı: " + alan);
+
+    }
+}
