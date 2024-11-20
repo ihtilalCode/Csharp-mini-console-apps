2cce3db1f60b1b0e21f43dbe336923cefc47be82
ihtilalCode <hilalnuraltuntas67@gmail.com>
2024-11-20 15:01:43 +0300

    Add files via upload
    Kullanıcıdan üç kenar uzunluğu alınan üçgenin çevresini hesaplar.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    { 
+        Console.WriteLine("Üçgenin 1. kenarını girin: ");
+        int kenar1 = Convert.ToInt32(Console.ReadLine());
+    
+        Console.WriteLine("Üçgenin 2. kenarını girin: ");
+        int kenar2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Üçgenin 3. kenarını girin: ");
+        int kenar3 = Convert.ToInt32(Console.ReadLine());   
+
+        int cevre = kenar1 + kenar2 + kenar3;
+
+        Console.WriteLine("Üçgenin çevresi: " + cevre );
+    }
+
+}
+
