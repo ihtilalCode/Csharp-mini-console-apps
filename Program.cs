9f7d64e44eb8f8d78ab8b11933b40d5c61b00751
ihtilalCode <hilalnuraltuntas67@gmail.com>
2024-11-20 17:06:20 +0300

    Add files via upload
    Öğrencinin iki sınav notunun ortalamasını alır.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string adSoyad = OgrenciBilgileriniAl();
+
+        int ortalama = NotHesapla();
+
+
+        Console.WriteLine($"{adSoyad} adlı öğrencinin not ortalaması: {ortalama}");
+     }
+
+    static string OgrenciBilgileriniAl()
+    {
+        Console.WriteLine("öğrenci adı ve soyadı: ");
+     }
+
+
+    static int NotHesapla()
+    {
+        Console.WriteLine("1. sınav notunu girin: ");
+        int not1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("2. sınav notunu girin: ");
+        int not2 = Convert.ToInt32(Console.ReadLine());
+
+        int ortalama = (not1 + not2) / 2;
+        return ortalama;    
+    }
+}
