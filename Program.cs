53775a89b0a3eabfc2497ab7c5aad96d74663486
ihtilalCode <hilalnuraltuntas67@gmail.com>
2024-11-20 17:23:29 +0300

    Add files via upload
    Ders notları girilen öğrencinin dersi geçip geçemediğini hesaplar.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{  
+static void Main()
+{
+    Console.WriteLine("1. ders notunu girin: ");
+    int not1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("2.ders notunu girin : ");
+    int not2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("3. ders notunu girin: ");
+    int not3 = Convert.ToInt32(Console.ReadLine());
+
+    double ortalama = (not1 + not2 + not3) / 3.0;
+
+    Console.WriteLine($"Ortalama: {ortalama}");
+    if (ortalama > 50)
+    {
+        Console.WriteLine("Dersi geçtiniz");
+
+    }
+    if (ortalama < 50)
+    {
+        Console.WriteLine("geçemediniz");
+    }
+}
+}
