4cfc08329084aedaa0508dc1b85cba5c6f0769e1
ihtilalCode <hilalnuraltuntas67@gmail.com>
2024-11-20 18:37:49 +0300

    Add files via upload
    Öğrenci veya öğretmen olma durumuna göre ayrı ayrı ders programı sunar.
    (Basit c# class denemesi)

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Lütfen rolünüzü seçin:");
+        Console.WriteLine("1. Öğrenci");
+        Console.WriteLine("2. Öğretmen");
+
+        string secim = Console.ReadLine();
+
+        if (secim == "1")
+        {
+            OgrenciDersleri();
+        }
+        else if (secim == "2")
+        {
+
+            OgretmenDersleri();
+        }
+        else
+        {
+            Console.WriteLine("Geçersiz seçim. Lütfen 1 veya 2'yi girin.");
+        }
+    }
+
+    static void OgrenciDersleri()
+    {
+        Console.WriteLine("Seçtiğiniz dersler:");
+        Console.WriteLine("- Algoritma");
+        Console.WriteLine("- Yazılım Tasarım");
+    }
+
+    static void OgretmenDersleri()
+    {
+        Console.WriteLine("Seçtiğiniz dersler:");
+        Console.WriteLine("- Elektronik");
+        Console.WriteLine("- Fizik");
+    }
+}
+
+
+
+
+
