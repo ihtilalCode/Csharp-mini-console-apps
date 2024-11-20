11071eef6b46d7a73a58ce3fb2fa327b3cd91605
ihtilalCode <hilalnuraltuntas67@gmail.com>
2024-11-20 14:29:08 +0300

    Add files via upload
    Öğrenciden alınan ad, soyad, telefon ve öğrenci no bilgileriyle kimlik oluşturur.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Adınızı girin: "); //kullanıcı metin girer.
+        string ad = Console.ReadLine();  //girilen metni string değişkenine atar.
+
+        Console.Write("Soyadınızı girin: "); // aynı işlemler tekrarlanır.
+        string soyad = Console.ReadLine();
+
+        Console.Write("Öğrenci numaranızı girin: ");
+        string ogrenciNo = Console.ReadLine();
+
+        Console.Write("Telefon numaranızı girin: ");
+        string telefonNo = Console.ReadLine();
+
+        Console.WriteLine("\nKullanıcı Bilgileri:"); //konsola satır yazdırılır.
+        Console.WriteLine($"Ad: {ad}");             //kullanıcıdan alınan bilgiler {} içinde konsola yazdırılır.
+        Console.WriteLine($"Soyad: {soyad}");
+        Console.WriteLine($"Öğrenci No: {ogrenciNo}");
+        Console.WriteLine($"Telefon No: {telefonNo}");
+
+    }
+}
