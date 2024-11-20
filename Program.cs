b4a4cf08f5bf06f01698c74d13205dec1ab0b717
ihtilalCode <hilalnuraltuntas67@gmail.com>
2024-11-20 18:07:16 +0300

    Add files via upload
     Kendisi hariç pozitif tam bölenlerinin toplamı kendisine eşit olan sayı süper sayıdır.
    Girilen sayının süper sayı olup olmadığını bildirir.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma_10.hafta_1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bir sayı giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            int bolentoplam = 0;
+
+            for (int i = 1; i < sayi; i++) 
+            {
+                if (sayi % i == 0)
+                {
+                    bolentoplam+=i;
+                }
+            }
+
+            if (bolentoplam == sayi)
+            {
+                Console.WriteLine("Süper sayıdır.");
+            }
+            else
+            {
+                Console.WriteLine("Süper sayı değildir. ");
+            }
+
+
+        }
+    }
+}
